65624993bc967c99c5c4c10610c7ce82fc553b31
miaan15 <miaanstarman@gmail.com>
2026-05-11 11:12:04 +0700

    Revert "remove this->"
    This reverts commit 82c18483d0384ac93e1f6673209cbfd53d10d35c.

--- src/core/strat/slotmap.cpp
+++ src/core/strat/slotmap.cpp
@@ -56,62 +56,62 @@ export template <typename T, typename Alloc = std::allocator<T>> struct SlotMap 
     }
 
     auto destroy() const {
-        auto slot_alloc = slots.get_allocator();
+        auto slot_alloc = this->slots.get_allocator();
 
-        if (data_raw != nullptr) {
+        if (this->data_raw != nullptr) {
             using TAlloc = typename std::allocator_traits<SlotAlloc>::template rebind_alloc<T>;
             TAlloc t_alloc(slot_alloc);
-            std::allocator_traits<TAlloc>::deallocate(t_alloc, data_raw, cap);
+            std::allocator_traits<TAlloc>::deallocate(t_alloc, this->data_raw, this->cap);
         }
 
-        if (data_slot_index_raw != nullptr) {
+        if (this->data_slot_index_raw != nullptr) {
             using SizeAlloc = typename std::allocator_traits<SlotAlloc>::template rebind_alloc<size_t>;
             SizeAlloc size_alloc(slot_alloc);
-            std::allocator_traits<SizeAlloc>::deallocate(size_alloc, data_slot_index_raw, cap);
+            std::allocator_traits<SizeAlloc>::deallocate(size_alloc, this->data_slot_index_raw, this->cap);
         }
     }
 
     [[nodiscard]] auto push(const T &val) -> SlotMapKey {
-        handle_append_data(val, next_slot_index);
+        handle_append_data(val, this->next_slot_index);
 
-        if (next_slot_index == slots.size()) {
-            slots.push_back(SlotMapKey{.index = next_slot_index + 1, .gen = 0});
+        if (this->next_slot_index == this->slots.size()) {
+            this->slots.push_back(SlotMapKey{.index = this->next_slot_index + 1, .gen = 0});
         }
 
-        SlotMapKey *slot_ptr = &slots[next_slot_index];
-        size_t index = next_slot_index;
+        SlotMapKey *slot_ptr = &this->slots[this->next_slot_index];
+        size_t index = this->next_slot_index;
 
-        next_slot_index = slot_ptr->index;
-        slot_ptr->index = len - 1;
+        this->next_slot_index = slot_ptr->index;
+        slot_ptr->index = this->len - 1;
 
         return SlotMapKey{.index = index, .gen = slot_ptr->gen};
     }
 
     auto remove(const SlotMapKey &key) -> bool {
-        if (key.index >= slots.size()) return false;
+        if (key.index >= this->slots.size()) return false;
 
-        SlotMapKey *slot_ptr = &slots[key.index];
+        SlotMapKey *slot_ptr = &this->slots[key.index];
         if (slot_ptr->gen != key.gen) return false;
 
         size_t data_index = slot_ptr->index;
-        T *data_ptr = &data_raw[data_index];
-        T *last_data_ptr = &data_raw[len - 1];
+        T *data_ptr = &this->data_raw[data_index];
+        T *last_data_ptr = &this->data_raw[this->len - 1];
 
         if (data_ptr != last_data_ptr) {
-            size_t last_data_slot_index = data_slot_index_raw[len - 1];
-            SlotMapKey *last_data_slot_ptr = &slots[last_data_slot_index];
+            size_t last_data_slot_index = this->data_slot_index_raw[this->len - 1];
+            SlotMapKey *last_data_slot_ptr = &this->slots[last_data_slot_index];
 
             *data_ptr = *last_data_ptr;
 
             last_data_slot_ptr->index = data_index;
 
-            data_slot_index_raw[data_index] = last_data_slot_index;
+            this->data_slot_index_raw[data_index] = last_data_slot_index;
         }
 
         handle_pop_data();
 
-        slot_ptr->index = next_slot_index;
-        next_slot_index = key.index;
+        slot_ptr->index = this->next_slot_index;
+        this->next_slot_index = key.index;
 
         ++slot_ptr->gen;
 
@@ -120,39 +120,39 @@ export template <typename T, typename Alloc = std::allocator<T>> struct SlotMap 
 
     auto set(const SlotMapKey &key, const T &val) -> bool {
         size_t index = key.index;
-        if (index >= slots.size()) return false;
+        if (index >= this->slots.size()) return false;
 
-        SlotMapKey slot = slots[index];
+        SlotMapKey slot = this->slots[index];
         if (key.gen != slot.gen) return false;
 
-        data_raw[slot.index] = val;
+        this->data_raw[slot.index] = val;
         return true;
     }
 
     [[nodiscard]] auto get(const SlotMapKey &key) const -> std::optional<T> {
         size_t index = key.index;
-        if (index >= slots.size()) return {};
+        if (index >= this->slots.size()) return {};
 
-        SlotMapKey slot = slots[index];
+        SlotMapKey slot = this->slots[index];
         if (key.gen != slot.gen) return {};
 
-        return data_raw[slot.index];
+        return this->data_raw[slot.index];
     }
     [[nodiscard]] auto get_ptr(const SlotMapKey &key) -> std::optional<T *> {
         size_t index = key.index;
-        if (index >= slots.size()) return {};
+        if (index >= this->slots.size()) return {};
 
-        SlotMapKey slot = slots[index];
+        SlotMapKey slot = this->slots[index];
         if (key.gen != slot.gen) return {};
 
-        return &data_raw[slot.index];
+        return &this->data_raw[slot.index];
     }
 
     [[nodiscard]] auto has(const SlotMapKey &key) const -> bool {
         size_t index = key.index;
-        if (index >= slots.size()) return false;
+        if (index >= this->slots.size()) return false;
 
-        SlotMapKey slot = slots[index];
+        SlotMapKey slot = this->slots[index];
         if (key.gen != slot.gen) return false;
 
         return true;
@@ -161,33 +161,33 @@ export template <typename T, typename Alloc = std::allocator<T>> struct SlotMap 
     auto clear() {
         handle_clear_data();
 
-        for (size_t i = 0; i < slots.size(); i++) {
-            ++slots[i].gen;
-            slots[i].index = i + 1;
+        for (size_t i = 0; i < this->slots.size(); i++) {
+            ++this->slots[i].gen;
+            this->slots[i].index = i + 1;
         }
-        next_slot_index = 0;
+        this->next_slot_index = 0;
     }
 
     auto reserve(size_t cap) {
         handle_reserve_data(cap);
-        slots.reserve(cap);
+        this->slots.reserve(cap);
     }
 
-    [[nodiscard]] auto size() -> size_t { return len; }
-    [[nodiscard]] auto capacity() -> size_t { return cap; }
+    [[nodiscard]] auto size() -> size_t { return this->len; }
+    [[nodiscard]] auto capacity() -> size_t { return this->cap; }
 
-    auto begin() -> iterator { return data_raw; }
-    auto end() -> iterator { return data_raw + len; }
-    auto begin() const -> const_iterator { return data_raw; }
-    auto end() const -> const_iterator { return data_raw + len; }
-    auto cbegin() const -> const_iterator { return data_raw; }
-    auto cend() const -> const_iterator { return data_raw + len; }
+    auto begin() -> iterator { return this->data_raw; }
+    auto end() -> iterator { return this->data_raw + this->len; }
+    auto begin() const -> const_iterator { return this->data_raw; }
+    auto end() const -> const_iterator { return this->data_raw + this->len; }
+    auto cbegin() const -> const_iterator { return this->data_raw; }
+    auto cend() const -> const_iterator { return this->data_raw + this->len; }
 
 private:
     auto handle_reserve_data(size_t cap) {
-        if (cap <= cap) return;
+        if (cap <= this->cap) return;
 
-        auto slot_alloc = slots.get_allocator();
+        auto slot_alloc = this->slots.get_allocator();
         using TAlloc = typename std::allocator_traits<SlotAlloc>::template rebind_alloc<T>;
         using SizeAlloc = typename std::allocator_traits<SlotAlloc>::template rebind_alloc<size_t>;
 
@@ -197,35 +197,35 @@ private:
         T *new_data_raw = std::allocator_traits<TAlloc>::allocate(t_alloc, cap);
         size_t *new_data_slot_index_raw = std::allocator_traits<SizeAlloc>::allocate(size_alloc, cap);
 
-        if (data_raw != nullptr) {
-            std::memcpy(new_data_raw, data_raw, len * sizeof(T));
-            std::allocator_traits<TAlloc>::deallocate(t_alloc, data_raw, cap);
+        if (this->data_raw != nullptr) {
+            std::memcpy(new_data_raw, this->data_raw, this->len * sizeof(T));
+            std::allocator_traits<TAlloc>::deallocate(t_alloc, this->data_raw, this->cap);
         }
-        if (data_slot_index_raw != nullptr) {
-            std::memcpy(new_data_slot_index_raw, data_slot_index_raw, len * sizeof(size_t));
-            std::allocator_traits<SizeAlloc>::deallocate(size_alloc, data_slot_index_raw, cap);
+        if (this->data_slot_index_raw != nullptr) {
+            std::memcpy(new_data_slot_index_raw, this->data_slot_index_raw, this->len * sizeof(size_t));
+            std::allocator_traits<SizeAlloc>::deallocate(size_alloc, this->data_slot_index_raw, this->cap);
         }
 
-        data_raw = new_data_raw;
-        data_slot_index_raw = new_data_slot_index_raw;
-        cap = cap;
+        this->data_raw = new_data_raw;
+        this->data_slot_index_raw = new_data_slot_index_raw;
+        this->cap = cap;
     }
 
     auto handle_append_data(const T &val, size_t slot_index) {
-        if (len + 1 > cap) {
-            size_t new_cap = cap * 2;
+        if (this->len + 1 > this->cap) {
+            size_t new_cap = this->cap * 2;
             if (new_cap < 4) new_cap = 4;
             handle_reserve_data(new_cap);
         }
 
-        data_raw[len] = val;
-        data_slot_index_raw[len] = slot_index;
+        this->data_raw[this->len] = val;
+        this->data_slot_index_raw[this->len] = slot_index;
 
-        ++len;
+        ++this->len;
     }
 
-    auto handle_pop_data() { --len; }
-    auto handle_clear_data() { len = 0; }
+    auto handle_pop_data() { --this->len; }
+    auto handle_clear_data() { this->len = 0; }
 };
 
 } // namespace cactus
