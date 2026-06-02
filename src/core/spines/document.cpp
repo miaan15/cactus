cc78c13a76c259cf295db037d4c8d76ff44173dc
miaan15 <miaanstarman@gmail.com>
2026-06-02 09:44:25 +0700

    spines document add pick() to access data

--- src/core/spines/document.cpp
+++ src/core/spines/document.cpp
@@ -20,29 +20,37 @@ export struct Token {
 
 export struct SpinesDocument {
     using data_t = std::variant<int, float, unsigned int>;
-    struct IdentifierData {
+    struct IdentifierNameView {
+        size_t offset_root;
+        size_t len;
+    };
+    struct IdentifierOwnedData {
         size_t point_to;
         size_t data_len;
         size_t parent_len;
     };
+    struct IdentifierData {
+        IdentifierNameView name;
+        IdentifierOwnedData owned_data;
+    };
+
+    std::string spines_source;
 
     DynamicArray<Token> tokens;
 
     DynamicArray<data_t> data;
     DynamicArray<char> string_data;
     DynamicArray<IdentifierData> identifier_data_list;
-    HashMap<std::string, size_t> identifier_to_id_map;
-    // TODO: futher optimize by make a buffer to store all identifier's name since it should be all mostly static
 
     size_t next_identifier_id = 0;
     size_t next_string_data_index = 0;
 
     [[nodiscard]] static auto make() noexcept -> SpinesDocument {
-        return SpinesDocument{.tokens = DynamicArray<Token>::make(),
+        return SpinesDocument{.spines_source{},
+                              .tokens = DynamicArray<Token>::make(),
                               .data = DynamicArray<data_t>::make(),
                               .string_data = DynamicArray<char>::make(),
                               .identifier_data_list = DynamicArray<IdentifierData>::make(),
-                              .identifier_to_id_map = HashMap<std::string, size_t>::make(),
                               .next_identifier_id = 0,
                               .next_string_data_index = 0};
     }
@@ -51,19 +59,18 @@ export struct SpinesDocument {
         data.destroy();
         string_data.destroy();
         identifier_data_list.destroy();
-        identifier_to_id_map.destroy();
     }
     [[nodiscard]] auto clone() = delete; // FIXME
 
     auto parse(const stdf::path &dir) {
-        std::string content = get_and_preprocess_file(dir).value(); // TODO error
-        std::println("string:\n{}\n", content);
+        spines_source = get_and_preprocess_file(dir).value(); // TODO error
+        std::println("string:\n{}\n", spines_source);
 
-        handle_lexer(content);
+        handle_lexer(spines_source);
 
         for (auto p : tokens) {
             std::println("index {} at {}:{} : token = {}; len = {} >> {}", p.index, p.line, p.column, (size_t)p.type, p.len,
-                         std::string_view(content.data() + p.index, p.len));
+                         std::string_view(spines_source.data() + p.index, p.len));
         }
 
         std::stack<size_t> identifier_stack{};
@@ -73,7 +80,7 @@ export struct SpinesDocument {
 
             _assert(identifier_stack.top() < identifier_data_list.len,
                     "identifier_stack.top() should be store index to identifier_data_list which means is in bound.");
-            IdentifierData old_identifier_data = identifier_data_list.get(identifier_stack.top()).value();
+            IdentifierOwnedData old_identifier_data = identifier_data_list.get(identifier_stack.top()).value().owned_data;
             size_t old_data_len = old_identifier_data.data_len;
             size_t old_parent_len = old_identifier_data.parent_len;
 
@@ -83,26 +90,24 @@ export struct SpinesDocument {
 
             _assert(identifier_stack.top() < identifier_data_list.len,
                     "identifier_stack.top() should be store index to identifier_data_list which means is in bound.");
-            IdentifierData *identifier_data = identifier_data_list.get_ptr(identifier_stack.top());
+            IdentifierOwnedData *identifier_data = &identifier_data_list.get_ptr(identifier_stack.top())->owned_data;
             identifier_data->data_len += old_data_len;
             identifier_data->parent_len += old_parent_len;
         };
         for (auto token : tokens) {
             switch ((int)token.type) {
             case TokenType::IDENTIFIER: {
-                std::string str = content.substr(token.index, token.len);
-                identifier_to_id_map.add(std::move(str), next_identifier_id);
-                identifier_data_list.append({.point_to = data.len, .data_len = 0, .parent_len = 1});
+                identifier_data_list.append(IdentifierData{
+                    .name = {token.index, token.len}, .owned_data = {.point_to = data.len, .data_len = 0, .parent_len = 1}});
 
                 identifier_stack.push(next_identifier_id);
-
                 ++next_identifier_id;
 
-                just_after_identifier = 2; // cheat flag by set = 2 then decrease 2 times (because I dont want to somehow exit
-                                           // this case without decrease this flag value)
+                just_after_identifier = 2; // NOTE: cheat flag by set = 2 then decrease 2 times (because I dont want to somehow
+                                           // exit this case without decrease this flag value)
             } break;
             case TokenType::NUMBER: {
-                std::string_view strv(content.data() + token.index, token.len);
+                std::string_view strv(spines_source.data() + token.index, token.len);
 
                 int i_val;
                 auto res_i = std::from_chars(strv.data(), strv.data() + strv.size(), i_val);
@@ -126,11 +131,11 @@ export struct SpinesDocument {
                 _assert(!identifier_stack.empty(), "identifier should not be empty yet");
                 _assert(identifier_stack.top() < identifier_data_list.len,
                         "identifier_stack.top() should be store index to identifier_data_list which means is in bound.");
-                ++identifier_data_list.get_ptr(identifier_stack.top())->data_len;
+                ++identifier_data_list.get_ptr(identifier_stack.top())->owned_data.data_len;
                 if (just_after_identifier) pop_identifier_stack();
             } break;
             case TokenType::STRING: {
-                std::string_view strv(content.data() + token.index, token.len);
+                std::string_view strv(spines_source.data() + token.index, token.len);
 
                 data.append((unsigned int)next_string_data_index);
 
@@ -142,7 +147,7 @@ export struct SpinesDocument {
                 _assert(!identifier_stack.empty(), "identifier should not be empty yet");
                 _assert(identifier_stack.top() < identifier_data_list.len,
                         "identifier_stack.top() should be store index to identifier_data_list which means is in bound.");
-                ++identifier_data_list.get_ptr(identifier_stack.top())->data_len;
+                ++identifier_data_list.get_ptr(identifier_stack.top())->owned_data.data_len;
                 if (just_after_identifier) pop_identifier_stack();
             } break;
             case '{': {
@@ -173,10 +178,10 @@ export struct SpinesDocument {
         std::println();
 
         std::println("identifier:");
-        for (auto [name, id] : identifier_to_id_map) {
-            _assert(id < identifier_data_list.len, "id should be store index to identifier_data_list which means is in bound.");
-            IdentifierData data = identifier_data_list.get(id).value();
-            std::println("{}: point_to: {}; data_len: {}; parent_len: {}", name, data.point_to, data.data_len, data.parent_len);
+        for (auto data : identifier_data_list) {
+            std::println("{}: point_to: {}; data_len: {}; parent_len: {}",
+                         std::string_view(spines_source.data() + data.name.offset_root, data.name.len),
+                         data.owned_data.point_to, data.owned_data.data_len, data.owned_data.parent_len);
         }
         std::println();
     }
@@ -397,6 +402,100 @@ private:
         }
 
         content_view->remove_prefix(removed);
+    }
+
+public:
+    struct Accessor {
+        SpinesDocument const *doc_ref = nullptr;
+        size_t current_id = 0;
+        std::optional<size_t> target_data_index = {}; // has value -> in a identifier, no value -> in a access value
+
+        [[nodiscard]] auto pick(std::string_view search_name) const noexcept -> Accessor {
+            _assert(doc_ref != nullptr, "Document reference is null.");
+            _assert(!target_data_index.has_value(), "Cannot call .pick(name) on a data value.");
+            _assert(current_id < doc_ref->identifier_data_list.len, "current_id is out of bounds.");
+
+            auto current_data = doc_ref->identifier_data_list.get(current_id).value();
+
+            size_t search_start = current_id + 1;
+            size_t search_end = current_id + current_data.owned_data.parent_len;
+
+            _assert(search_end <= doc_ref->identifier_data_list.len, "Search range exceeds identifier_data_list bounds.");
+
+            for (size_t i = search_start; i < search_end; ++i) {
+                auto child_data = doc_ref->identifier_data_list.get(i).value();
+
+                _assert(child_data.name.offset_root + child_data.name.len <= doc_ref->spines_source.size(),
+                        "String view out of bounds of spines_source.");
+
+                std::string_view child_name{doc_ref->spines_source.data() + child_data.name.offset_root, child_data.name.len};
+
+                if (child_name == search_name) { return Accessor{doc_ref, i, {}}; }
+            }
+
+            std::println("Error: Child identifier '{}' not found in this scope.", search_name);
+            _assert(false, "Child identifier not found.");
+            return *this;
+        }
+
+        [[nodiscard]] auto pick(size_t index) const noexcept -> Accessor {
+            _assert(doc_ref != nullptr, "Document reference is null.");
+            _assert(!target_data_index.has_value(), "Cannot chain .pick(index) on a data value.");
+            _assert(current_id < doc_ref->identifier_data_list.len, "current_id is out of bounds.");
+
+            auto current_data = doc_ref->identifier_data_list.get(current_id).value();
+            _assert(index < current_data.owned_data.data_len, "Data index out of bounds for this identifier.");
+
+            _assert(current_data.owned_data.point_to + index < doc_ref->data.len,
+                    "Calculated data index out of bounds of data container.");
+
+            return Accessor{doc_ref, current_id, current_data.owned_data.point_to + index};
+        }
+
+        template <typename T> [[nodiscard]] auto as() const noexcept -> std::optional<T> {
+            _assert(doc_ref != nullptr, "Document reference is null.");
+            _assert(target_data_index.has_value(), "Accessor points to an identifier, not a value. Call .pick(index) first.");
+            if (!target_data_index.has_value()) return {}; // TODO error handle
+
+            _assert(target_data_index.value() < doc_ref->data.len, "Target data index should be in bound");
+            auto val = doc_ref->data.get(target_data_index.value()).value();
+
+            if constexpr (std::is_same_v<T, std::string_view> || std::is_same_v<T, std::string>) {
+                _assert(std::holds_alternative<unsigned int>(val), "Requested string, but data is not a string index.");
+                if (!std::holds_alternative<unsigned int>(val)) return {}; // TODO error handle
+
+                unsigned int str_idx = std::get<unsigned int>(val);
+                _assert(str_idx < doc_ref->string_data.len, "String index out of bounds of string_data container.");
+
+                return std::string_view(doc_ref->string_data.get_ptr(str_idx));
+
+            } else if constexpr (std::is_same_v<T, int>) {
+                _assert(std::holds_alternative<int>(val), "Requested int, but data is not an int.");
+                if (!std::holds_alternative<int>(val)) return {}; // TODO error handle
+
+                return std::get<int>(val);
+
+            } else if constexpr (std::is_same_v<T, float>) {
+                _assert(std::holds_alternative<float>(val), "Requested float, but data is not a float.");
+                if (!std::holds_alternative<float>(val)) return {}; // TODO error handle
+
+                return std::get<float>(val);
+
+            } else {
+                static_assert(sizeof(T) == 0, "Unsupported type cast for .as<T>()");
+            }
+        }
+    };
+
+    [[nodiscard]] auto pick(std::string_view root_name) const noexcept -> Accessor {
+        for (size_t i = 0; i < identifier_data_list.len; i += identifier_data_list.get(i).value().owned_data.parent_len) {
+            auto data = identifier_data_list.get(i).value();
+            std::string_view name{spines_source.data() + data.name.offset_root, data.name.len};
+            if (name == root_name) return Accessor{this, i, {}};
+        }
+        std::println("Error: Root identifier '{}' not found.", root_name);
+        _assert(false, "Root identifier not found.");
+        return Accessor{this, 0, {}};
     }
 };
 
--- test/core/bundle_test.cpp
+++ test/core/bundle_test.cpp
@@ -4,8 +4,38 @@ import cactus;
 using namespace cactus;
 
 int main() {
-
     SpinesDocument a = SpinesDocument::make();
     a.parse("test/core/idk.bundle");
+
+    std::println("\n--- Testing Accessor Navigation ---");
+
+    // 1. Deeply nested integer lookup
+    // Path: a -> aa -> aaa -> [0]
+    auto val_aaa = a.pick("a").pick("aa").pick("aaa").pick(0).as<int>();
+    if (val_aaa) { std::println("1. Nested Int: a.aa.aaa = {}", val_aaa.value()); }
+
+    // 2. Deeply nested string lookup
+    // Path: a -> aa -> aac -> [0]
+    auto val_aac = a.pick("a").pick("aa").pick("aac").pick(0).as<std::string_view>();
+    if (val_aac) { std::println("2. Nested Str: a.aa.aac = \"{}\"", val_aac.value()); }
+
+    // 3. Array elements lookup
+    // Path: a -> ac -> [0, 1, 2]
+    auto node_ac = a.pick("a").pick("ac");
+    std::println("3. Array Vals: a.ac[0] = {}", node_ac.pick(0).as<int>().value_or(-1));
+    std::println("               a.ac[1] = {}", node_ac.pick(1).as<int>().value_or(-1));
+    std::println("               a.ac[2] = {}", node_ac.pick(2).as<int>().value_or(-1));
+
+    // 4. Mixed block lookup (unnamed data point alongside a named child identifier)
+    // Path: a -> ad -> [0] (the '4') AND a -> ad -> flag -> [0] (the '5')
+    auto node_ad = a.pick("a").pick("ad");
+    std::println("4. Mixed Blck: a.ad[0] (unnamed) = {}", node_ad.pick(0).as<int>().value_or(-1));
+
+    auto val_flag = node_ad.pick("flag").pick(0).as<int>();
+    if (val_flag) { std::println("               a.ad.flag = {}", val_flag.value()); }
+
+    std::println("-----------------------------------\n");
+
     a.destroy();
+    return 0;
 }
